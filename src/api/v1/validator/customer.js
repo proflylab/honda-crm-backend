19e1fc11eda919480d31abc9e3bd4bb8076e30fa
proflylab <proflylab@pm.me>
2023-05-25 09:41:14 +0700

    fix customer validate

--- src/api/v1/validator/customer.js
+++ src/api/v1/validator/customer.js
@@ -7,15 +7,15 @@ const validator = {}
 
 validator.create = [
   body('name').notEmpty().withMessage('name is required').trim(),
-  body('type').notEmpty().withMessage('firstName is required').trim(),
-  body('mobilePhone').notEmpty().withMessage('lastName is required').trim(),
+  body('type').notEmpty().withMessage('type is required').trim(),
+  body('mobilePhone').notEmpty().withMessage('mobilePhone is required').trim(),
   validateResult
 ]
 
 validator.update = [
   body('name').notEmpty().withMessage('name is required').trim(),
-  body('type').notEmpty().withMessage('firstName is required').trim(),
-  body('mobilePhone').notEmpty().withMessage('lastName is required').trim(),
+  body('type').notEmpty().withMessage('type is required').trim(),
+  body('mobilePhone').notEmpty().withMessage('mobilePhone is required').trim(),
   validateResult
 ]
 
